1356ac648fceac08bc936fb103182d56129c4533
Sergiy <ssvnauka@gmail.com>
2025-12-30 00:27:44 +0200

    Create ConsultationRequest.js

---
+++ src/models/ConsultationRequest.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const consultationRequestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  topic: {
+    type: String,
+    required: true
+  },
+  problemDescription: {
+    type: String,
+    required: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('ConsultationRequest', consultationRequestSchema);
